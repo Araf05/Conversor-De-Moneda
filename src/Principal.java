b50165805854273a2227ad2fef7ce179c39df6d3
Araceli Noemí Fernández <aracelifernandez.3006@gmail.com>
2024-05-11 16:47:42 -0300

    Switch, opciones del menu

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,6 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import io.araf.desafioConversorDeMonedas.modelos.ConsultaConversionDeMoneda;
 import io.araf.desafioConversorDeMonedas.modelos.Moneda;
 
@@ -6,11 +9,13 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
         ConsultaConversionDeMoneda consulta = new ConsultaConversionDeMoneda();
 
-        Moneda moneda = consulta.buscarMoneda("USD" , "ARS");
-        System.out.println(moneda);
-        // reales , ARS, USD, POLICÍA
+        // BRL , ARS, USD, COP
         String menu = """
                 **************************
                 MENU
@@ -25,6 +30,45 @@ public class Principal {
                 Elija una opcion válida: 
                 **************************
                 """;
+        while (true) {
+            System.out.println(menu);
+            Moneda moneda = null;
+            int opcion = Integer.parseInt(lectura.nextLine());
+            switch (opcion) {
+                case 1:
+                    moneda = consulta.buscarMoneda("USD" , "ARS");
+                    System.out.println(gson.toJson(moneda));
+                    break;
+                case 2:
+                    moneda = consulta.buscarMoneda("ARS" , "USD");
+                    System.out.println(gson.toJson(moneda));
+                    break;
+                case 3:
+                    moneda = consulta.buscarMoneda("USD" , "BRL");
+                    System.out.println(gson.toJson(moneda));
+                    break;
+                case 4:
+                    moneda = consulta.buscarMoneda("BRL" , "USD");
+                    System.out.println(gson.toJson(moneda));
+                    break;
+                case 5:
+                    moneda = consulta.buscarMoneda("USD" , "COP");
+                    System.out.println(gson.toJson(moneda));
+                    break;
+                case 6:
+                    moneda = consulta.buscarMoneda("COP" , "USD");
+                    System.out.println(gson.toJson(moneda));
+                    break;
+                case 7:
+                    System.out.println("La aplicación está finalizando");
+                    return;
+                default:
+                    System.out.println("Opción invalida.");
+                    break;
+            }
+
+        }
+
 
     }
 }
